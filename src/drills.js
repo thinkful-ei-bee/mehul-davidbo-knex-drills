25818f2cb5f0574097909f31174b4b34945479f0
Mehul Patel <mehulpatel118@gmail.com>
2019-04-09 11:35:09 -0400

    finished drills

--- src/drills.js
+++ src/drills.js
@@ -12,10 +12,52 @@ console.log('connection successful');
 
 //Drill 1
 
-knexInstance
-  .select('*')
-  .from('amazong_products')
-  .where({ name: 'Point of view gun' })
-  .then(result => {
-    console.log(result)
-  })
+function searchName (searchTerm){
+  knexInstance
+    .select('*')
+    .from('shopping_list')
+    .where('name', 'ILIKE', `%${searchTerm}%` )
+    .then(result => {
+      console.log(result);
+    });
+}
+
+
+function itemPaginated (pageNumber){
+  knexInstance
+    .select('*')
+    .from('shopping_list')
+    .limit(6)
+    .offset(6*(pageNumber-1))
+    .then(result => {
+      console.log(result);
+    });
+}
+  
+function afterDate(daysAgo) {
+  knexInstance
+    .select('*')
+    .from('shopping_list')
+    .where('date_added',
+      '>',
+      knexInstance.raw('now() - \'?? days\'::INTERVAL', daysAgo))
+    .then(result => {
+      console.log(result);
+    });
+}
+
+
+
+
+function totalCost(){
+  knexInstance
+    .select('category')
+    .sum('price')
+    .from('shopping_list')
+    .groupBy('category')
+    .then(result => {
+      console.log(result);
+    });
+}
+
+totalCost();
